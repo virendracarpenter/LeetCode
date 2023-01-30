dac4994896cc5813a5ced0e1d8bb84cbb96e245f
Virendra Carpenter <66617140+virendracarpenter@users.noreply.github.com>
2023-01-30 18:55:32 +0530

    Time: 3 ms (38.83%), Space: 5.9 MB (94.16%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -3,9 +3,9 @@ public:
     int tribonacci(int n) {
         if (n == 0)
             return 0;
-        int t0 = 0;
-        int t1 = 1;
-        int t2 = 1;
+        int t0, t1, t2;
+        t0 = 0;
+        t1 = t2 = 1;
         for (int i = 0; i < n-2; i++){
             int tn = t0 + t1 + t2;
             t0 = t1;
