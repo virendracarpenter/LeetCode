5c2470676a50db9f79eb1e05908867f103b909a7
Virendra Carpenter <66617140+virendracarpenter@users.noreply.github.com>
2023-02-22 21:06:05 +0530

    Time: 54 ms (78.72%), Space: 26.2 MB (37.41%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,36 @@
+#define v vector<int>
+class Solution {
+public:
+    bool feasible(v& weights, int c, int days) {
+        int daysNeeded = 1, currentLoad = 0;
+        for (int weight : weights) {
+            currentLoad += weight;
+            if (currentLoad > c) {
+                daysNeeded++;
+                currentLoad = weight;
+            }
+        }
+
+        return daysNeeded <= days;
+    }
+
+    int shipWithinDays(v& weights, int days) {
+        int totalLoad = 0, maxLoad = 0;
+        for (int weight : weights) {
+            totalLoad += weight;
+            maxLoad = max(maxLoad, weight);
+        }
+
+        int l = maxLoad, r = totalLoad;
+
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (feasible(weights, mid, days)) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+};
